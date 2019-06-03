8e403182497c54d47af8f3e2101fe200d121755c
Annie Truex <artruex@gmail.com>
2019-06-03 00:58:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
   
   def apply_discount
     if @discount > 0
-      @to_take_off = (price * discount)/100
+      @to_take_off = (@price * discount)/100
       @total -= @to_take_off
       return "After the discount, the total comes to $#{total}."
     else
